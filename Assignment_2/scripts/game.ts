886e05f2e26bbda312dd2b74a95be540e0fe76ec
Adam Zhao <adamtieao@gmail.com>
2015-02-22 22:03:27 -0500

    Finish adding seven buttons

--- Assignment_2/scripts/game.ts
+++ Assignment_2/scripts/game.ts
@@ -1,23 +1,27 @@
 ﻿// CreateJS Boilerplate for COMP397
+
+
 // VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-var canvas;
-var stage;
+var canvas; // Reference to the HTML 5 Canvas element
+var stage: createjs.Stage; // Reference to the Stage
+
 
 // GAME OBJECTS
-var game;
-var btnBet1;
-var btnBet5;
-var btnBet10;
-var btnBet100;
-var background;
-var btnSpin;
-var betMaxButton;
-var betOneButton;
-var resetButton;
-var powerButton;
+var game: createjs.Container; // Main Game Container Object
+var background: createjs.Bitmap;
+var spinButton: createjs.Bitmap;
+var betMaxButton: createjs.Bitmap;
+var betOneButton: createjs.Bitmap;
+var resetButton: createjs.Bitmap;
+var powerButton: createjs.Bitmap;
+
 
 // FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 function init() {
+
+
+
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas); // Parent Object
     stage.enableMouseOver(20); // Turn on Mouse Over events
@@ -27,6 +31,7 @@ function init() {
 
     main();
 }
+
 
 // GAMELOOP
 function gameLoop() {
@@ -38,67 +43,32 @@ function spinButtonClicked() {
 }
 
 function spinButtonOut() {
-    spinButton.alpha = 1; // 100% Alpha
+    spinButton.alpha = 1; // 100% Alpha 
+
 }
 
 function spinButtonOver() {
     spinButton.alpha = 0.7;
+
 }
 
 function createUI() {
+
     background = new createjs.Bitmap("images/slot machine-V3.png");
     game.addChild(background); // Add the background to the game container
 
-    
-    //Reset Button
-    btnReset = new createjs.Bitmap("images/btnReset.png");
-    game.addChild(btnReset);
-    btnReset.x = 53;
-    btnReset.y = 481;
-    
-    //Bet+1 Button
-    btnBet1 = new createjs.Bitmap("images/btnBet1.png");
-    game.addChild(btnBet1);
-    btnBet1.x = 118;
-    btnBet1.y = 481;
-
-    //Bet+5 Button
-    btnBet5 = new createjs.Bitmap("images/btnBet5.png");
-    game.addChild(btnBet5);
-    btnBet5.x = 183;
-    btnBet5.y = 481;
-
-    //Bet+10 Button
-    btnBet10 = new createjs.Bitmap("images/btnBet10.png");
-    game.addChild(btnBet10);
-    btnBet10.x = 248;
-    btnBet10.y = 481;
-
-    //Bet+100 Button
-    btnBet100 = new createjs.Bitmap("images/btnBet100.png");
-    game.addChild(btnBet100);
-    btnBet100.x = 313;
-    btnBet100.y = 481;
-
-    //Bet Max Button
-    btnBetMax = new createjs.Bitmap("images/btnBetMax.png");
-    game.addChild(btnBetMax);
-    btnBetMax.x = 378;
-    btnBetMax.y = 481;
-    
-
-
     // Spin Button
-    btnSpin = new createjs.Bitmap("images/btnSpin.png");
-    game.addChild(btnSpin);
-    btnSpin.x = 475;
-    btnSpin.y = 229;
+    spinButton = new createjs.Bitmap("assets/images/spinButton.png");
+    game.addChild(spinButton);
+    spinButton.x = 410;
+    spinButton.y = 545;
 
-    /*
     // Spin Button Event Listeners
     spinButton.addEventListener("click", spinButtonClicked);
     spinButton.addEventListener("mouseover", spinButtonOver);
     spinButton.addEventListener("mouseout", spinButtonOut);
+
+
 
     // Bet Max Button
     betMaxButton = new createjs.Bitmap("assets/images/betMaxButton.png");
@@ -123,8 +93,9 @@ function createUI() {
     game.addChild(powerButton);
     powerButton.x = 55;
     powerButton.y = 560;
-    */
+
 }
+
 
 function main() {
     game = new createjs.Container(); // Instantiates the Game Container
@@ -132,5 +103,9 @@ function main() {
     createUI();
 
     stage.addChild(game); // Adds the Game Container to the Stage
+    
+
 }
-//# sourceMappingURL=game.js.map
+
+
+
